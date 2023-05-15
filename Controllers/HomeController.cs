6bdee57f35f2fd2accf4b3ea24228cf02eeae542
Tim Holleque <timothy.t.holleque@gmail.com>
2023-05-15 17:06:52 -0400

    Itssss WOrRkIng-Anakin Skywalker

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using DeckOfCardsAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardsAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        CardDAL deckAPI = new CardDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Deck deck = deckAPI.GetDeck();
+            Deck cards = deckAPI.GetHand(deck.deck_id);
+            return View(cards);
+        }
+
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/CardDAL.cs
@@ -0,0 +1,29 @@
+﻿using RestSharp;
+
+
+
+namespace DeckOfCardsAPI.Models
+{
+    public class CardDAL
+    {
+        public Deck GetDeck()
+        {
+            string URL = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+            RestClient client = new RestClient(URL);
+            RestRequest request = new RestRequest();
+            Deck response = client.Get<Deck>(request);
+
+            return response; 
+        }
+        public Deck GetHand(string id)
+        {
+            string URL = $"https://deckofcardsapi.com/api/deck/{id}/draw/?count=5";
+            RestClient client = new RestClient(URL);
+            RestRequest request = new RestRequest();
+            Deck response = client.Get<Deck>(request);
+
+            return response;
+        }
+        
+    }
+}
